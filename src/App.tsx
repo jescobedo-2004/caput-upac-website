ad89eb29f41eb0cc5789aadbbba222d73fe64c36
francisco jesus manzor barboza <116230724+l4s7h0r@users.noreply.github.com>
2025-09-15 20:41:41 -0300

    Subiendo cambios de scroll y secciones

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,23 @@ export function App() {
   return (
     <>
       <Header />
-      <main>
-        <Inicio />
-        <Proyectos />
-        <Residencias />
-        <CharlasYTalleres />
-        <Contacto />
-      </main>
+      <div className="relative">
+        <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-green-200 to-blue-200">
+          <Inicio />
+        </div>
+        <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-indigo-800 to-purple-800 text-white">
+          <Proyectos />
+        </div>
+        <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-purple-800 to-pink-800 text-white">
+          <Residencias />
+        </div>
+        <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-200 to-indigo-100 text-black">
+          <CharlasYTalleres />
+        </div>
+        <div className="sticky top-0 h-screen flex flex-col items-center justify-center bg-gradient-to-b from-green-200 to-blue-200">
+          <Contacto />
+        </div>
+      </div>
     </>
   );
 }
